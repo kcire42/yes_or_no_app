eec10065519381649ed204e46559a855e9e5851b
Kcire42 <erickcarrillo2807@gmail.com>
2025-09-23 21:23:33 -0600

    Instalar funcionamiento teclado

--- lib/presentation/screen/widgets/shared/message_field_box.dart
+++ lib/presentation/screen/widgets/shared/message_field_box.dart
@@ -5,21 +5,44 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final focusNode = FocusNode();
+    final textController = TextEditingController();
     final outputlineborder = OutlineInputBorder(
       borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(30.0),
     );
+    final inputdecoration = InputDecoration(
+      enabledBorder: outputlineborder,
+      focusedBorder: outputlineborder,
+      hintText: 'Type your message',
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {
+          final text = textController.value.text;
+          if (text.isEmpty) {
+            print('no message to send');
+            return;
+          } else {
+            print('send message as $text using button');
+            textController.clear();
+          }
+        },
+      ),
+    );
 
     return TextFormField(
-      decoration: InputDecoration(
-        enabledBorder: outputlineborder,
-        focusedBorder: outputlineborder,
-        filled: true,
-        suffixIcon: IconButton(
-          icon: const Icon(Icons.send_outlined),
-          onPressed: () {},
-        ),
-      ),
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputdecoration,
+      onFieldSubmitted: (value) {
+        print('submited value $value using keyboard');
+        textController.clear();
+        focusNode.requestFocus();
+      },
     );
   }
 }
